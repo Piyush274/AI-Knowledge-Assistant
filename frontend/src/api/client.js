0c3849f08ea6cdb1c21867b646de1625568999a5
Piyush274 <pksangam888@gmail.com>
2026-07-16 22:20:33 +0530

    feat: initialize Axios client with JWT interceptor and configure Vite proxy for backend integration

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -4,3 +4,63 @@ const client = axios.create({
     baseURL: "/api",
 })
 
+// axios.get("/api/documents") is same as client.get("/documents")
+
+//FastAPI backend can then extract and verify this token
+
+// Before every request, Axios gives you the request configuration object.
+client.interceptors.request.use((config) => {
+
+    const token = localStorage.getItem("token");
+
+    // This ensures you only add the Authorization header when the user is logged in.
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+
+    // If null request is sent without an authenticationn header
+
+    return config;
+
+})
+
+//biggest advantage of using an Axios instance with an interceptor is that you write the authentication logic once, and every request made through client automatically includes the JWT token if the user is logged in.
+
+// React Component
+//        │
+//        ▼
+// client.get("/documents")
+//        │
+//        ▼
+// Axios Instance
+// (baseURL = "/api")
+//        │
+//        ▼
+// Request Interceptor
+//        │
+//        ├── Read token from localStorage
+//        ├── If present, add:
+//        │      Authorization: Bearer <token>
+//        ▼
+// Request becomes:
+// GET /api/documents
+//        │
+//        ▼
+// Vite Proxy
+//        │
+//        ▼
+// GET http://localhost:8000/documents
+//        │
+//        ▼
+// FastAPI Backend
+//        │
+//        ▼
+// Response
+//        │
+//        ▼
+// Axios
+//        │
+//        ▼
+// React Component
+export default client;
+
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
 
-  })
+  //Any frontend request beginning with /api will be proxied, to backend server url
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    },
+  },
+})
